d184f96124cb1f1c247eaeabb10c5f352159b46f
Ryan Bales <thinkt4nk@gmail.com>
2011-08-13 15:34:19 -0500

    clean up

--- EJSON.php
+++ EJSON.php
@@ -2,7 +2,7 @@
 
 /**
  * EJSON 
- * 	Extends CJSON to include HPack algorithm for objects and arrays
+ * 	Extends CJSON to include HPack algorithm for arrays of objects and associative arrays
  *  See: http://web-resource-optimization.blogspot.com/2011/06/json-compression-algorithms.html 
  * @uses CJSON
  * @version 0.1
@@ -12,56 +12,37 @@ class EJSON extends CJSON
 {
 	public static function hpack($var)
 	{
-		if( is_array($var) || is_object($var) )
+		if( is_array($var) && (count($var) > 0) ) 
 		{
-			if( is_array($var) ) // Array
+			$first_var = $var[0];
+			$hpack_list = array();
+			// Associative Array
+			if( is_array($first_var) && count($first_var) && (array_keys($first_var) !== range(0,sizeof($first_var) -1)) )
 			{
-				if( (count($var) > 0) )
+				$hpack_list[] = array_keys($first_var);
+				foreach( $var as $v )
 				{
-					$first_var = $var[0];
-					$hpack_list = array();
-					if( is_array($first_var) && count($first_var) && (array_keys($first_var) !== range(0,sizeof($first_var) -1)) )
-					{
-						$hpack_list[] = array_keys($first_var);
-						foreach( $var as $v )
-						{
-							$hpack_list[] = array_values($v);
-						}
-					}
-					elseif( is_object($first_var) && ($first_var instanceof Traversable) )
-					{
-						$keys = array();
-						foreach( $first_var as $k=>$v ) {
-							$keys[] = $k;
-						}
-						$hpack_list[] = $keys;
-						foreach( $var as $v ) {
-							$var_values = array();
-							foreach( $v as $key => $value ) {
-								$var_values[] = $value;
-							}
-							$hpack_list[] = $var_values;
-						}
-					}
-					return self::encode($hpack_list);
+					$hpack_list[] = array_values($v);
 				}
-			} 
-			else { // Object
-				if ($var instanceof Traversable)
-				{
-					$vars = array();
-					foreach ($var as $k=>$v)
-						$vars[$k] = $v;
-				}
-				else
-					$vars = get_object_vars($var);
-				return '[' . 
-					'[' . join(',', array_map(array('CJSON', 'encode'), array_keys($vars))) . ']' .
-					'[' . join(',', array_map(array('CJSON', 'encode'), array_values($vars))) . ']' . 
-				']';
 			}
-
-		}
+			// Object
+			elseif( is_object($first_var) && ($first_var instanceof Traversable) )
+			{
+				$keys = array();
+				foreach( $first_var as $k=>$v ) {
+					$keys[] = $k;
+				}
+				$hpack_list[] = $keys;
+				foreach( $var as $v ) {
+					$var_values = array();
+					foreach( $v as $key => $value ) {
+						$var_values[] = $value;
+					}
+					$hpack_list[] = $var_values;
+				}
+			}
+			return self::encode($hpack_list);
+		} 
 		return self::encode($var);
 	}
 }
